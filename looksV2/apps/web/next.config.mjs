e7afd085436ce8f05d9885ea72f9a3504fb2b42a
afzalstudio123-wq <afzalstudio123@gmail.com>
2026-07-03 03:13:46 +0530

    Add files via upload

---
+++ looksV2/apps/web/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If the site is deployed to a subpath (e.g. /repository-name), uncomment and set basepath:
+  // basePath: '/lakhnavi-sewa-in',
+};
+
+export default nextConfig;
